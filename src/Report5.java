49b252e02252bc2f9544912cf31365a04488f0d8
Asato Fukuhara <e205730@ie.u-ryukyu.ac.jp>
2022-02-25 01:47:51 +0900

    2nd commit

---
+++ src/Report5.java
@@ -0,0 +1,13 @@
+
+
+public class Report5{
+    public static void main(String[] args){
+        try{
+            String str =  null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("エラーが発生しました。：" + e.getMessage());
+        }
+    }
+}
